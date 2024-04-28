0e032206a3db30f48e2e32e8b89955aebc95f701
Lepronet85 <abdoulazizsoumana85@gmail.com>
2024-04-28 22:17:21 +0100

    Update engine files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the class DBStorage
+This script encapsulates the DBStorage class for database interactions.
 """
 
 import models
@@ -16,17 +16,21 @@ import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
+# Dictionary mapping class names to their corresponding classes
+class_map = {"Amenity": Amenity, "City": City,
+             "Place": Place, "Review": Review, "State": State, "User": User}
 
 class DBStorage:
-    """interaacts with the MySQL database"""
+    """
+    This class facilitates interactions with the MySQL database.
+    """
     __engine = None
     __session = None
 
     def __init__(self):
-        """Instantiate a DBStorage object"""
+        """
+        Constructs a DBStorage object, initializing the database connection.
+        """
         HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
         HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
         HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
@@ -41,36 +45,71 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """query on the current database session"""
+        """
+        Queries the current database session for all objects of a given class.
+        """
         new_dict = {}
-        for clss in classes:
-            if cls is None or cls is classes[clss] or cls is clss:
-                objs = self.__session.query(classes[clss]).all()
+        for clss in class_map:
+            if cls is None or cls is class_map[clss] or cls is clss:
+                objs = self.__session.query(class_map[clss]).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
         return (new_dict)
 
     def new(self, obj):
-        """add the object to the current database session"""
+        """
+        Adds an object to the current database session.
+        """
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
+        """
+        Commits all changes of the current database session.
+        """
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session obj if not None"""
+        """
+        Deletes an object from the current database session if it is not None.
+        """
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """reloads data from the database"""
+        """
+        Reloads data from the database, resetting the session.
+        """
         Base.metadata.create_all(self.__engine)
         sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(sess_factory)
         self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
+        """
+        Removes the session from the current database session.
+        """
         self.__session.remove()
+
+    def get(self, cls, id):
+        """
+        Retrieves an object based on its class name and ID, or returns None if not found.
+        """
+        objects = self.__session.query(class_map[cls])
+        for obj in objects:
+            if obj.id == id:
+                return obj
+        return None
+
+    def count(self, cls=None):
+        """
+        Counts the number of objects in storage matching a given class name.
+        If no class name is provided, counts all objects in storage.
+        """
+        nobjects = 0
+        for clss in class_map:
+            if cls is None or cls is class_map[clss] or cls is clss:
+                nobjects += len(self.__session.query(class_map[clss]).all())
+        return nobjects
+
+
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Contains the FileStorage class
+This script encapsulates the FileStorage class for serializing and deserializing instances to/from a JSON file.
 """
 
 import json
@@ -12,20 +12,23 @@ from models.review import Review
 from models.state import State
 from models.user import User
 
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
+# Dictionary mapping class names to their corresponding classes
+class_map = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+             "Place": Place, "Review": Review, "State": State, "User": User}
 
 class FileStorage:
-    """serializes instances to a JSON file & deserializes back to instances"""
-
+    """
+    This class handles the serialization of instances to a JSON file and the deserialization back to instances.
+    """
     # string - path to the JSON file
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
 
     def all(self, cls=None):
-        """returns the dictionary __objects"""
+        """
+        Returns the dictionary __objects, filtered by class if specified.
+        """
         if cls is not None:
             new_dict = {}
             for key, value in self.__objects.items():
@@ -35,36 +38,69 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """sets in __objects the obj with key <obj class name>.id"""
+        """
+        Adds an object to the __objects dictionary with the key format <obj class name>.id.
+        """
         if obj is not None:
             key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
+        """
+        Serializes the __objects dictionary to the JSON file specified by __file_path.
+        """
         json_objects = {}
         for key in self.__objects:
-            json_objects[key] = self.__objects[key].to_dict()
+            json_objects[key] = self.__objects[key].to_dict(dump="Yes")
         with open(self.__file_path, 'w') as f:
             json.dump(json_objects, f)
 
     def reload(self):
-        """deserializes the JSON file to __objects"""
+        """
+        Deserializes the JSON file to the __objects dictionary.
+        """
         try:
             with open(self.__file_path, 'r') as f:
                 jo = json.load(f)
             for key in jo:
-                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+                self.__objects[key] = class_map[jo[key]["__class__"]](**jo[key])
         except:
             pass
 
     def delete(self, obj=None):
-        """delete obj from __objects if it’s inside"""
+        """
+        Removes an object from the __objects dictionary if it exists.
+        """
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             if key in self.__objects:
                 del self.__objects[key]
 
     def close(self):
-        """call reload() method for deserializing the JSON file to objects"""
+        """
+        Calls the reload() method to deserialize the JSON file to objects.
+        """
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Retrieves an object based on its class name and ID, or returns None if not found.
+        """
+        key = "{}.{}".format(cls, id)
+        if key in self.__objects.keys():
+            return self.__objects[key]
+        return None
+
+    def count(self, cls=None):
+        """
+        Counts the number of objects in storage matching a given class name.
+        If no class name is provided, counts all objects in storage.
+        """
+        if cls:
+            counter = 0
+            for obj in self.__objects.values():
+                if obj.__class__.__name__ == cls:
+                    counter += 1
+            return counter
+        return len(self.__objects)
+
